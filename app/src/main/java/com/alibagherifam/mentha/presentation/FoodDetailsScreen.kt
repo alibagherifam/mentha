68ec274b389b37b154ab2cbd68ed0fe61ef1fdc7
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-01-12 18:04:56 +0330

    Extract a function for sample food creation

--- app/src/main/java/com/alibagherifam/mentha/presentation/FoodDetailsScreen.kt
+++ app/src/main/java/com/alibagherifam/mentha/presentation/FoodDetailsScreen.kt
@@ -241,22 +241,7 @@ fun Nutrition(
 fun FoodDetailsScreenPreview() {
     AppTheme {
         FoodDetailsScreen(
-            Food(
-                id = "orange",
-                name = "پرتقال",
-                summary = "پرتقال منبع خوبی از ویتامین C و پتاسیم است که هر دو می‌توانند به کاهش فشار خون کمک کنند. ویتامین C موجود در پرتقال یک آنتی اکسیدان قوی است که در حفظ جوانی پوست بسیار مهم بوده و در ترمیم سلولی و بهبود زخم نقش دارد. پرتقال سرشار از فیبر است؛ به طوری که یک پرتقال متوسط، 11 درصد نیاز روزانه‌ی بدن را تامین می‌کند.",
-                icon = R.drawable.img_banana,
-                weight = 140,
-                measure = "عدد",
-                energy = 65.8f,
-                water = 121f,
-                protein = 1.27f,
-                carbohydrate = 16.5f,
-                fat = 0.21f,
-                dietaryFiber = 2.8f,
-                sugar = 12f,
-                sodium = 12.6f
-            ),
+            getSampleFood(),
             onBackPressed = {},
             onShareClick = {}
         )
