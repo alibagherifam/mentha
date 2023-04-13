e6520710970812ddf345bc4204d9f075ebf9753a
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-04-14 00:46:38 +0430

    Exclude licence of libraries from final APK

--- build-logic/src/main/kotlin/common/AndroidConfiguration.kt
+++ build-logic/src/main/kotlin/common/AndroidConfiguration.kt
@@ -3,12 +3,15 @@ package common
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.kotlin.dsl.dependencies
 
 fun Project.configureAndroidBaseOptions(android: CommonExtension<*, *, *, *>) {
     val libs = getVersionCatalogs()
     setSdkVersionBoundary(android, libs)
     setJvmTargetVersion(android)
+    // desugarJdkLibraries(android, libs)
     configureWithDetekt(libs)
+    excludeLicencesFromApk(android)
 }
 
 fun setSdkVersionBoundary(android: CommonExtension<*, *, *, *>, libs: VersionCatalog) {
@@ -19,5 +22,27 @@ fun setSdkVersionBoundary(android: CommonExtension<*, *, *, *>, libs: VersionCat
             minSdk = libs.getRequiredVersion("androidMinSdk").toInt()
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         }
+    }
+}
+
+fun Project.desugarJdkLibraries(
+    android: CommonExtension<*, *, *, *>,
+    libs: VersionCatalog
+) {
+    android.compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    dependencies {
+        add(
+            "coreLibraryDesugaring",
+            libs.findLibrary("android.desugarJdkLibs").get()
+        )
+    }
+}
+
+private fun excludeLicencesFromApk(android: CommonExtension<*, *, *, *>) {
+    android.packaging.resources {
+        excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
 }
